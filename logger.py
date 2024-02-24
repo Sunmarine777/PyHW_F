245b207f35539a95873eeb60ba7eca564ffffd5a
Sunmarine <Sunmarine777@yandex.ru>
2024-02-24 19:38:37 +0300

    before changing entry

--- logger.py
+++ logger.py
@@ -31,31 +31,82 @@ def input_data():
 def print_data():
       var = int(input(f"What format of data were applied?\n"
       f"Option 1: \n"
-      f"'name'\n'surname'\n'phone''\n'address'\n'rec_num1'\n\n"
+      f"name\nsurname\nphone\naddress\nentry_number\n\n"
       f"Option 2: \n"
-      f"name;surname;phone;address;rec_num2\n"
+      f"name;surname;phone;address;entry_number\n"
       f"Choose option: "))
       while var != 1 and var != 2:
             print("Wrong input")
             var = int(input('Input number: '))
       if var == 1:
             with open('data1.csv', 'r', encoding='utf-8') as f:
-                  data_first = f.readlines()
-                  data_first_list = []
-            var1 = int(input(f"Input your entry number\n"))
-            j = 0
-            for i in range(len((data_first))):
-                  if i > var1*6-6 and i < var1*6:
-                        data_first_list.append(''.join(data_first[j:i+1]))
-                        j=i
-            print(''.join(data_first_list))
+                  data_1 = f.readlines()
+            data_first = []
+            var1 = int(input(f"Input your entry number: "))
+            if var1*6 > len(data_1):
+                  print("Wrong input")
+            for i in range(1):
+                  data_first.append(''.join(data_1[var1*6-6:var1*6-1]))
+            print(''.join(data_first))
+
       elif var == 2:
             with open('data2.csv', 'r', encoding='utf-8') as f:
-                  data_second = f.readlines()
-                  var2 = int(input(f"Input your entry number\n"))
-                  entry2 = ()
-                  for i in range(data_second):
-                        if i == var2*2:
-                              entry2 = data_second[i]
-                  print(*entry2)
+                  data_2 = f.readlines()
+            var2 = int(input(f"Input your entry number: "))
+            if var2*2-2 > len(data_2):
+                  print("Wrong input")
+            if var2 == 1:
+                  print(data_2[0])
+            for i in enumerate(data_2):                        
+                  if i == var2*2-2:
+                        break
+                  pass
+            print(data_2[var2*2-2])
+            
+    
+def change_data():
+      var_c = str(input(f'Enter \'Y\' to change your entry:\n'))
+      while var_c != 'Y':
+            print("Wrong input")
+            var_c = int(input('Input number: '))
+      if var_c == 'Y':
+            var = int(input(f"What format of data were applied?\n"
+            f"Option 1: \n"
+            f"name\nsurname\nphone\naddress\nentry_number\n\n"
+            f"Option 2: \n"
+            f"name;surname;phone;address;entry_number\n"
+            f"Choose option: "))
+      name = name_data()
+      surname = surname_data()
+      phone = phone_data()
+      address = address_data()
+      rec_num1 = record_number1()
+      rec_num2 = record_number2()     
+      while var != 1 and var != 2:
+            print("Wrong input")
+            var = int(input('Input number: '))
+      if var == 1:
+            with open('data1.csv', 'r+', encoding='utf-8') as f:
+                  data_1 = f.readlines()
+            data_first = []
+            var1 = int(input(f"Input your entry number: "))
+            if var1*6 > len(data_1):
+                  print("Wrong input")
+            for i in range(1):
+                  data_first.append(''.join(data_1[var1*6-6:var1*6-1]))
+
+      elif var == 2:
+            with open('data2.csv', 'r+', encoding='utf-8') as f:
+                  data_2 = f.readlines()
+            var2 = int(input(f"Input your entry number: "))
+            if var2*2-2 > len(data_2):
+                  print("Wrong input")
+            if var2 == 1:
+                  with open('data2.csv', 'r+', encoding='utf-8') as f:
+                        f[0].write(f"{name};{surname};{phone};{address};{rec_num2}\n")
+                  print(*f[0])
+            with open('data2.csv', 'r+', encoding='utf-8') as f:
+                  f[var2*2-2].write(f"{name};{surname};{phone};{address};{rec_num2}\n")
+            print(*f[var2*2-2])
+
 
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from logger import input_data, print_data
+from logger import input_data, print_data, change_data
 
 
 def interface():
-      print('Good day! You are in the bot mode \n 1 - enter data \n 2 - receive data')
+      print('Good day! You are in the bot mode \n 1 - enter data \n 2 - receive data \n 3 - change data')
       command = int(input('Enter number: '))
       
-      while command != 1 and command != 2:
+      while command != 1 and command != 2 and command != 3:
             print('Wrong input')
             command = int(input('Enter  number: '))
-                 
+
       if command == 1:
             input_data()
       elif command == 2:
             print_data()
+      elif command == 3:
+            change_data()
 
 
 if  __name__ == '__main__':
